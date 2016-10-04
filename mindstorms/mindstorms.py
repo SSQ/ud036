ca6a97c62490f7e32dbc22abfc9844d4420ea8a2
CadenShen <ssq6554@gmail.com>
2016-10-04 20:58:06 +0800

    draw square and circle

---
+++ mindstorms/mindstorms.py
@@ -0,0 +1,31 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+    
+    brad = turtle.Turtle()
+
+    brad.shape("circle")
+    brad.color("yellow")
+    brad.speed(4)
+    
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+
+    angie = turtle.Turtle()
+
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.circle(100)
+    
+
+    window.exitonclick()
+    
+draw_square()
