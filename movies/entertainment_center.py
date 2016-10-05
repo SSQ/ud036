e7bb010aa08a083b728977fb447b6d2d8a7da1e6
CadenShen <ssq6554@gmail.com>
2016-10-05 16:12:37 +0800

    toy-story-Class-Movie

---
+++ movies/entertainment_center.py
@@ -0,0 +1,7 @@
+import media
+
+toy_story = media.Movie("T S",
+                        "A story about ",
+                        "http://",
+                        "https://")
+print(toy_story.storyline)
