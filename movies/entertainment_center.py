2d0dfab871a26a55640f45de26bbf61960df0408
CadenShen <ssq6554@gmail.com>
2016-10-05 17:34:28 +0800

    class-variable

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,7 +1,13 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("T S",
                         "A story about ",
                         "http://",
-                        "https://")
-print(toy_story.storyline)
+                        "https://github.com/")
+#print(toy_story.storyline)
+
+#toy_story.show_trailer()
+movies = [toy_story]
+#fresh_tomatoes.open_movies_page(movies)
+print(media.Movie.VALID_RATINGS)
