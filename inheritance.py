87d75906a9f6e4152f67e5d554a8cd1b3f401c80
CadenShen <ssq6554@gmail.com>
2016-10-05 19:29:10 +0800

    method-overriding

--- inheritance.py
+++ inheritance.py
@@ -14,9 +14,15 @@ class Child(Parent):
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
 
+    def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye Color - " + self.eye_color)
+        print("Number of toys - " + str(self.number_of_toys))
+
 billy_cyrus = Parent("Cyrus", "blue")
-billy_cyrus.show_info()
+#billy_cyrus.show_info()
 #print(billy_cyrus.last_name)
-#miley_cyrus = Child("Cyrus", "Blue", 5)
+miley_cyrus = Child("Cyrus", "Blue", 5)
+miley_cyrus.show_info()
 #print(miley_cyrus.last_name)
 #print(miley_cyrus.number_of_toys)
